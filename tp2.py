5046c5f7d8d244b00f0daa7ade06501fa4467510
Vicky Taussig <mvtaussig@udesa.edu.ar>
2025-10-15 13:49:47 -0300

    new

--- tp2.py
+++ tp2.py
@@ -1,2 +1,7 @@
-def main():
-    print("hola")
+import os
+from PIL import Image
+import numpy as np
+
+def cargar_imagen(ruta):
+    img = Image.open(ruta).convert("RGB")
+    return np.array(img, dtype=np.uint8)
